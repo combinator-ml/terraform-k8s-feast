10ca30916da2c66bf02c49aa7f776ec2798f63f6
Phil Winder <phil@WinderResearch.com>
2021-05-17 14:16:10 +0100

    fix: service in wrong namespace

--- examples/testfaster/main.tf
+++ examples/testfaster/main.tf
@@ -1,18 +1,27 @@
+variable "namespace" {
+  description = "(Optional) The namespace to install into. Defaults to feast."
+  type        = string
+  default     = "feast"
+}
+
 module "feast" {
-  source = "combinator-ml/feast/k8s"
+  source    = "combinator-ml/feast/k8s"
+  namespace = var.namespace
 }
 
 resource "kubernetes_service" "jupyter" {
   metadata {
-    name = "testfaster-jupyter"
+    name      = "testfaster-jupyter"
+    namespace = var.namespace
   }
   spec {
     selector = {
       app = "feast-jupyter"
     }
     port {
-      port        = 31888
+      port        = 8888
       target_port = 8888
+      node_port   = 31888
     }
     type = "NodePort"
   }
